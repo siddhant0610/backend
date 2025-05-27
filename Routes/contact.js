2debb772f78eeda99a05066e160a4bfcec83c91c
Siddhant Sharma <siddhant4040s@gmail.com>
2025-05-27 17:32:06 +0530

    display message

--- Routes/contact.js
+++ Routes/contact.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../Models/contactMessage');
+const Contact = require('../Models/ContactMessage');
 const { body, validationResult } = require('express-validator');
 const rateLimit = require('express-rate-limit');
 const nodemailer = require('nodemailer');
